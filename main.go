e8e038a88a4f07964dc8237eae2b667c9be041d9
Ben Knáb <bkiac@pm.me>
2021-08-27 20:38:53 +0200

    Fix main.go conflict

--- main.go
+++ main.go
@@ -1,28 +1,8 @@
 package main
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import (
 	"fmt"
 )
-
-func main() {
-	res, err := GetMovie("https://www.imdb.com/title/tt0469494/")
-	if err != nil {
-		fmt.Println(err)
-	}
-<<<<<<< HEAD
-	fmt.Println(res.Search)
-=======
-func main() {
-	GetBook("https://www.goodreads.com/book/show/2175.Madame_Bovary")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println(res)
->>>>>>> 0037e3e (Start goodreads scraper)
-=======
-import "fmt"
 
 func main() {
 	book, err := GetBook("https://www.goodreads.com/book/show/2175.Madame_Bovary")
@@ -40,12 +20,4 @@ func main() {
 		fmt.Println("Movie")
 		fmt.Println(movie)
 	}
-<<<<<<< HEAD
-	fmt.Println(book)
->>>>>>> 5e7b5f1 (Add return type to book getter)
-=======
->>>>>>> 94b4151 (Add movie print)
-=======
-	fmt.Println(res)
->>>>>>> a7a89b4 (Add movie getter by URL)
 }
