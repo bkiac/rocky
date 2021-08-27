94b4151b75e5ab6866a05fbf95ac189a63d832dc
Ben Knáb <bkiac@pm.me>
2021-08-27 20:07:51 +0200

    Add movie print

--- main.go
+++ main.go
@@ -6,6 +6,16 @@ func main() {
 	book, err := GetBook("https://www.goodreads.com/book/show/2175.Madame_Bovary")
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println("Book")
+		fmt.Println(book)
 	}
-	fmt.Println(book)
+
+	movie, err := GetMovie("https://www.imdb.com/title/tt0469494/")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Movie")
+		fmt.Println(movie)
+	}
 }
